f51fcd3e2922725f88dfcd586843746c6866743b
Clementine <dupycournau@gmail.com>
2022-04-28 16:27:12 +0200

    documentation for all classes and test

--- amazon_pages/cart_page.py
+++ amazon_pages/cart_page.py
@@ -6,16 +6,18 @@ from .base_page import BasePage
 
 
 class CartPage(BasePage):
+    """ Page of cart"""
     quantity_select = (By.CSS_SELECTOR, "select[name='quantity']")
 
     def set_quantity(self, quantity):
+        """ Set the quantity of the first item"""
         quantity_select_element = Select(
             self.wait.until(EC.visibility_of_element_located(self.quantity_select))
         )
         quantity_select_element.select_by_value(str(quantity))
-        return self
 
     def get_quantity(self):
+        """ Get the quantity of the first item"""
         quantity_select_element = Select(
             self.wait.until(EC.visibility_of_element_located(self.quantity_select))
         )
